bd6c520f7ca64f72499742955c0e58c1019029fe
Alexandre Fiori <afiori@sandvine.com>
2015-09-09 22:58:41 -0400

    Add support for rpm

--- Makefile
+++ Makefile
@@ -5,10 +5,16 @@ build:
 
 clean:
 	python setup.py clean -a
+	@rm -rf build
 
 install: build
-	install -m 0755 -d /usr/local/sandvine/loadable
-	install -m 0755 build/lib.*/*.so /usr/local/sandvine/loadable
-	install -m 0755 -d /usr/local/sandvine/etc
-	install -m 0444 sandscript.py /usr/local/sandvine/etc
-	install -m 0644 policy.py /usr/local/sandvine/etc
+	install -m 0755 -d $(DESTDIR)/usr/local/sandvine/loadable
+	install -m 0755 build/lib.*/*.so $(DESTDIR)/usr/local/sandvine/loadable
+	install -m 0755 -d $(DESTDIR)/usr/local/sandvine/etc
+	install -m 0444 sandscript.py $(DESTDIR)/usr/local/sandvine/etc
+	install -m 0644 policy.py $(DESTDIR)/usr/local/sandvine/etc
+
+rpm:
+	@(cd rpm && ./make.bash)
+
+.PHONY: rpm
---
+++ rpm/make.bash
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Copyright 2015 Sandvine Incorporated ULC. All rights reserved.
+# Use of this source code is governed by a license that can be
+# found in the LICENSE file.
+
+# This script builds an rpm package for the SDE.
+# Use `rpm -qip` or `rpm -qlp` afterwards to see its contents.
+
+#set -ex
+
+VERSION=1.0
+NAME=sandscripthon-${VERSION}
+
+ROOT=/tmp/rpmbuild.$$
+trap "rm -rf ${ROOT}" EXIT
+
+# maketar makes a gzipped tarball with the files that will be
+# in the final rpm package.
+function maketar() {
+	tmp=$1/tmp
+	DESTDIR=$tmp/${NAME} make -C .. clean install
+
+	pushd $tmp
+	tar czf ${NAME}.tar.gz $NAME
+	popd
+
+	mv $tmp/${NAME}.tar.gz $1
+	rm -rf $tmp
+}
+
+# makerpm builds the rpm package and saves it in the current directory.
+function makerpm() {
+	topdir=${ROOT}/top
+	mkdir -p ${topdir}/RPMS ${topdir}/BUILD ${topdir}/SOURCES
+
+	maketar ${topdir}/SOURCES
+
+	buildroot=${ROOT}/root
+	mkdir -p $buildroot || true
+
+	rpmbuild \
+		--target amd64-redhat-linux \
+		--define "_topdir ${topdir}" \
+		--buildroot ${buildroot} \
+		-bb spec
+
+	mv ${topdir}/RPMS/amd64/* .
+}
+
+makerpm
